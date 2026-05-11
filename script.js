0b45b1c57625f10296118fa196f8991556cad551
senzafrenicrew <senzafrenicrew@gmail.com>
2026-05-11 15:04:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let slides = document.querySelectorAll('.slide');
+
+let current = 0;
+
+function changeSlide(){
+
+  slides[current].classList.remove('active');
+
+  current++;
+
+  if(current >= slides.length){
+    current = 0;
+  }
+
+  slides[current].classList.add('active');
+}
+
+setInterval(changeSlide,4000);
